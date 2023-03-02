a3810f1cede2051e49e928325222360fe45b0903
Данила <danila.evdokimov.444@mail.ru>
2023-03-02 04:55:45 +0300

    improved the code style, took into account comments from homework 15

--- src/main/java/com/example/filmcatalog/service/DirectorService.java
+++ src/main/java/com/example/filmcatalog/service/DirectorService.java
@@ -6,5 +6,6 @@ import java.util.UUID;
 
 public interface DirectorService {
     Director getDirector(UUID directorUuid);
+
     Director saveDirector(Director director);
 }
--- src/main/java/com/example/filmcatalog/service/impl/DirectorServiceImpl.java
+++ src/main/java/com/example/filmcatalog/service/impl/DirectorServiceImpl.java
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
-public class DirectorServiceImpl implements DirectorService
-{
+public class DirectorServiceImpl implements DirectorService {
     private final DirectorRepository directorRepository;
 
 
@@ -21,11 +21,11 @@ public class DirectorServiceImpl implements DirectorService
     @Override
     public Director getDirector(UUID directorUuid) {
         Optional<Director> directorOptional = directorRepository.findById(directorUuid);
-        return directorOptional.orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+        return directorOptional.orElseThrow(ChangeSetPersister.NotFoundException::new);
     }
 
     @Override
     public Director saveDirector(Director director) {
-        return  directorRepository.save(director);
+        return directorRepository.save(director);
     }
 }
