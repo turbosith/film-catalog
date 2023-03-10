c7b8feaee4adfe0e0eb451a8d27598d79bfb4df8
Evdokimov <danila.evdokimov.444@mail.ru>
2023-03-10 04:35:41 +0300

    add MockMvc

--- src/test/java/com/example/filmcatalog/DirectorControllerTests.java
+++ src/test/java/com/example/filmcatalog/DirectorControllerTests.java
@@ -5,92 +5,79 @@ import com.example.filmcatalog.model.Director;
 import com.example.filmcatalog.model.Film;
 import com.example.filmcatalog.repository.DirectorRepository;
 import com.example.filmcatalog.service.DirectorService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.AssertionsForClassTypes;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DirectorControllerTests {
-    @LocalServerPort
-    private int port;
-    @Autowired
-    private TestRestTemplate testRestTemplate;
     @Autowired
     private DirectorRepository directorRepository;
     @Autowired
     private DirectorService directorService;
-    private static HttpHeaders httpHeaders;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @BeforeAll
-    public static void init() {
-        httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+    @AfterEach
+    public void resetDb() {
+        directorRepository.deleteAll();
     }
 
     @Test
-    void addDirectorTest() throws JsonProcessingException {
-        Director expectedDirector = new Director();
-        expectedDirector.setName("Timofey");
-        expectedDirector.setAge(19);
-        expectedDirector.setCountry("Russia");
-        expectedDirector.setFilmList(List.of(
-                createFilm("Transformers", GenreEnum.ACTION, 5),
-                createFilm("Shrek 1", GenreEnum.HORROR, 10),
-                createFilm("Shrek 3", GenreEnum.ROMANCE, 10)));
-        HttpEntity<String> httpEntity = new HttpEntity<>(new ObjectMapper().writeValueAsString(expectedDirector),
-                httpHeaders);
-        ResponseEntity<Director> directorResponseEntity =
-                testRestTemplate.exchange(String.format("http://localhost:%d/director", port),
-                        HttpMethod.POST, httpEntity, Director.class);
-        Director actualDirector = directorResponseEntity.getBody();
-
-
-        AssertionsForClassTypes.assertThat(directorResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertNotNull(actualDirector);
-        assertNotNull(directorRepository.findById(actualDirector.getUuid()));
-        assertNotNull(directorService.getDirector(actualDirector.getUuid()));
-        AssertionsForClassTypes.assertThat(actualDirector.getFilmList().size()).
-                isEqualTo(expectedDirector.getFilmList().size());
-        assertNotNull(directorRepository.findById(actualDirector.getUuid()));
-        assertThat(actualDirector.getName()).isEqualTo(expectedDirector.getName());
-        assertThat(actualDirector.getAge()).isEqualTo(expectedDirector.getAge());
-        assertThat(actualDirector.getCountry()).isEqualTo(expectedDirector.getCountry());
-
-    }
-
-    @Test
-    void findDirectorTest() {
-        Director expectedDirector = new Director();
-        expectedDirector.setName("Grisha");
-        expectedDirector.setAge(87);
-        expectedDirector.setCountry("USA");
-        expectedDirector.setFilmList(List.of(
+    public void saveDirectorTest() throws Exception {
+        Director director = new Director();
+        director.setName("Grisha");
+        director.setAge(87);
+        director.setCountry("USA");
+        director.setFilmList(List.of(
                 createFilm("Cars 1", GenreEnum.ACTION, 5),
                 createFilm("Cars 2", GenreEnum.HORROR, 10),
                 createFilm("Cars 3", GenreEnum.ROMANCE, 10)));
-        directorRepository.save(expectedDirector);
-        ResponseEntity<Director> directorResponseEntity =
-                testRestTemplate.getForEntity(String.format("http://localhost:%d/director", port)
-                        + "?directorUuid={directorUuid}", Director.class, expectedDirector.getUuid());
-        Director actualDirector = directorResponseEntity.getBody();
-        assertThat(directorResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(actualDirector).isEqualTo(expectedDirector);
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(
+                        post("/director")
+                                .content(objectMapper.writeValueAsString(director))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(jsonPath("$.name").value("Grisha"))
+                .andExpect(jsonPath("$.age").value(87))
+                .andExpect(jsonPath("$.country").value("USA"))
+                .andExpect(jsonPath("$.filmList").isArray())
+        ;
+    }
 
+    @Test
+    public void addDirectorTest() throws Exception {
+        UUID uuid = createTestDirector("Timofey", 19, "Russia").getUuid();
+        mockMvc.perform(
+                        get("/director?directorUuid={uuid}", uuid.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.uuid").value(uuid.toString()))
+                .andExpect(jsonPath("$.name").value("Timofey"))
+                .andExpect(jsonPath("$.age").value(19))
+                .andExpect(jsonPath("$.country").value("Russia"));
+    }
 
+    private Director createTestDirector(String name, int age, String country) {
+        Director director = new Director();
+        director.setName(name);
+        director.setAge(age);
+        director.setCountry(country);
+        return directorService.saveDirector(director);
     }
 
     private Film createFilm(String title, GenreEnum genre, int rating) {
@@ -101,4 +88,5 @@ public class DirectorControllerTests {
         return film;
 
     }
+
 }
--- src/test/java/com/example/filmcatalog/FilmControllerTests.java
+++ src/test/java/com/example/filmcatalog/FilmControllerTests.java
@@ -4,82 +4,76 @@ import com.example.filmcatalog.constants.GenreEnum;
 import com.example.filmcatalog.model.Film;
 import com.example.filmcatalog.repository.FilmRepository;
 import com.example.filmcatalog.service.FilmService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
 class FilmControllerTests {
-    @LocalServerPort
-    private int port;
-    @Autowired
-    private TestRestTemplate testRestTemplate;
     @Autowired
     private FilmRepository filmRepository;
     @Autowired
     private FilmService filmService;
-    private static HttpHeaders httpHeaders;
 
-    @BeforeAll
-    public static void init() {
-        httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @AfterEach
+    public void resetDb() {
+        filmRepository.deleteAll();
     }
 
     @Test
-    void addFilmTest() throws JsonProcessingException {
-        Film expectedFilm = new Film();
-        UUID uuid = UUID.randomUUID();
-        expectedFilm.setUuid(uuid);
-        expectedFilm.setGenre(GenreEnum.ROMANCE);
-        expectedFilm.setRating(9);
-        expectedFilm.setTitle("Star Wars: Revenge of the Sith (Episode III)");
-        HttpEntity<String> httpEntity =
-                new HttpEntity<>(new ObjectMapper().writeValueAsString(expectedFilm), httpHeaders);
-        ResponseEntity<Film> filmResponseEntity =
-                testRestTemplate.exchange(String.format("http://localhost:%d/film", port),
-                        HttpMethod.POST, httpEntity, Film.class);
-        Film actualFilm = filmResponseEntity.getBody();
-        assertNotNull(actualFilm);
-        assertThat(filmResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertNotNull(filmRepository.findById(actualFilm.getUuid()));
-        assertNotNull(filmService.getFilm(actualFilm.getUuid()));
-        assertThat(actualFilm.getTitle()).isEqualTo(expectedFilm.getTitle());
-        assertThat(actualFilm.getUuid()).isNotEqualTo(expectedFilm.getUuid());
-        assertThat(actualFilm.getGenre()).isEqualTo(expectedFilm.getGenre());
-        assertThat(actualFilm.getRating()).isEqualTo(expectedFilm.getRating());
-
+    public void saveFilmTest() throws Exception {
+        Film film = new Film();
+        film.setGenre(GenreEnum.ROMANCE);
+        film.setRating(9);
+        film.setTitle("Star Wars: Revenge of the Sith (Episode III)");
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(
+                        post("/film")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(jsonPath("$.title").value("Star Wars: Revenge of the Sith (Episode III)"))
+                .andExpect(jsonPath("$.rating").value(9))
+                .andExpect(jsonPath("$.genre").value(GenreEnum.ROMANCE.toString()))
+        ;
     }
 
     @Test
-    void findFilmTest() {
-        Film expectedFilm = new Film();
-        expectedFilm.setGenre(GenreEnum.ACTION);
-        expectedFilm.setRating(10);
-        expectedFilm.setTitle("Shrek 2");
-        filmRepository.save(expectedFilm);
-        ResponseEntity<Film> filmResponseEntity =
-                testRestTemplate.getForEntity(String.format("http://localhost:%d/film", port)
-                        + "?filmUuid={filmUuid}", Film.class, expectedFilm.getUuid());
-        Film actualFilm = filmResponseEntity.getBody();
-        assertThat(filmResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(actualFilm).isEqualTo(expectedFilm);
+    public void addFilmTest() throws Exception {
+        UUID uuid = createTestFilm("Shrek 2", 10, GenreEnum.ACTION).getUuid();
+        mockMvc.perform(
+                        get("/film?filmUuid={uuid}", uuid.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.uuid").value(uuid.toString()))
+                .andExpect(jsonPath("$.title").value("Shrek 2"))
+                .andExpect(jsonPath("$.rating").value(10))
+                .andExpect(jsonPath("$.genre").value(GenreEnum.ACTION.toString()));
+    }
 
-
+    private Film createTestFilm(String title, int rating, GenreEnum genre) {
+        Film film = new Film();
+        film.setTitle(title);
+        film.setRating(rating);
+        film.setGenre(genre);
+        return filmService.saveFilm(film);
     }
 
 
